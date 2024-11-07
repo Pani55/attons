62693c9f548b9efc82128b85c87595c3588f7075
Pani_55 <Dumbasss_rus@mail.ru>
2024-11-07 13:41:35 +0300

    initial

---
+++ users/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        verbose_name="Адрес электронной почты",
+        unique=True
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Номер телефона",
+        blank=True,
+        null=True
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        blank=True,
+        null=True)
+    photo = models.ImageField(
+        verbose_name="Фотография профиля",
+        upload_to="users/",
+        blank=True,
+        null=True
+    )
+    telegram_id = models.CharField(
+        max_length=30,
+        verbose_name="Telegram chat ID",
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
